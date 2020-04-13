d3c527673654ff9d80e02d7426173099f04bde5c
BalbiZone <amabalbi@gmail.com>
2020-04-13 16:27:05 -0300

    Update snake.py
    useless update, makes some stuff easier to read

--- snake.py
+++ snake.py
@@ -2,9 +2,9 @@ import pygame, random
 from pygame.locals import *
 
 def on_grid_random():
-    x = random.randint(0,590)
-    y = random.randint(0,590)
-    return (x//10 * 10, y//10 * 10)
+    x = random.randint(0,59)
+    y = random.randint(0,59)
+    return (x * 10, y * 10)
 
 def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
